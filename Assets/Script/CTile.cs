482e142e59e99fd692bc91f83fbfb76b239b1e04
VocalPark <ab68333@naver.com>
2016-05-24 05:49:56 +0900

    make a tileEditor by jinsung

--- Assets/Script/CTile.cs
+++ Assets/Script/CTile.cs
@@ -3,20 +3,23 @@ using System.Collections;
 using System.IO;
 public class CTile : MonoBehaviour {
     public GameObject player;
-    public GameObject startPos;
+    public GameObject tile;
+    Vector3 startPos;
+    Vector2 tileStart;
     public int numPerRow;
     public int numPerCol;
     public StreamReader fp = null;
     bool[] pTile;
-    Vector2 tileStart;
     float tileSize;
     int index;
     // Use this for initialization
     void Start () {
+        //Init bool array means isSafeTile.
         pTile = new bool[numPerRow * numPerCol];
-        print(pTile.Length);
         for (int i = 0; i < numPerRow * numPerCol; ++i)
             pTile[i] = false;
+
+        //set bool array with indexes read in "road.txt". 
         fp = File.OpenText("./road.txt");
         if (fp != null) {
             while( fp.Peek() >= 0 )
@@ -26,20 +29,22 @@ public class CTile : MonoBehaviour {
                 //Debug.Log("idx =" + num);
             }
         }
-        GameObject start = GameObject.Find("tileStart");
-        GameObject next = GameObject.Find("nextTile");
-        if ( start ) {
-            tileSize = next.transform.position.x - start.transform.position.x;
-            tileStart.x = start.transform.position.x - tileSize/2;
-            tileStart.y = start.transform.position.z - tileSize/2;
-        }
+
+        //set tile's startPos & size.
+        tileSize = tile.transform.localScale.z/numPerRow;
+        tileStart.x = tile.transform.localPosition.x - tile.transform.localScale.x/2 + tileSize/2;
+        tileStart.y = tile.transform.localPosition.z - tile.transform.localScale.z/2 + tileSize/2;
+        startPos.x = tile.transform.localPosition.x;
+        startPos.z = tileStart.y;
+        startPos.y = player.transform.localPosition.y;
 	}
 	// Update is called once per frame
 	void Update () {
-        index = (int)((player.transform.position.z - tileStart.y)/tileSize) * numPerRow
-            + (int)((player.transform.position.x - tileStart.x)/tileSize);
-        if( index >= 0 && index <= pTile.Length-1 && !pTile[index] ) {
-            player.transform.position = startPos.transform.position;
+        index = (int)( ( player.transform.localPosition.x - tileStart.x ) / tileSize ) * numPerRow
+            + (int)( ( player.transform.localPosition.z - tileStart.y ) / tileSize );
+        if( index >= 0 && index <= pTile.Length - 1 && !pTile[index] ) {
+            player.transform.localPosition = startPos;
+            print("out");
         }
-	}
+    }
 }
---
+++ Assets/Script/CTileEditor.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class CTileEditor : MonoBehaviour {
+    StreamWriter fp = null;
+    public GameObject tile;
+    GameObject[] cube = new GameObject[11*15];
+    bool[] isSafe = new bool[11 * 15];
+    Vector3 mousePos;
+    Vector2 startPos;
+    float tileSize;
+    int index = 0;
+    int prevIdx = -1;
+    // Use this for initialization
+    void Start () {
+        tileSize = tile.transform.localScale.z / 15;
+        startPos.x = tile.transform.localPosition.x - tile.transform.localScale.x / 2;
+        startPos.y = tile.transform.localPosition.z - tile.transform.localScale.z / 2;
+        for( int i = 0 ; i < cube.Length ; ++i ) {
+            string findStr = "Text (" + i.ToString() + ")";
+            cube[i] = GameObject.Find(findStr);
+            Vector3 pos = new Vector3(startPos.x + tileSize / 2 + tileSize * (i % 11), 0.01f
+                                    , startPos.y + tileSize / 2 + tileSize * (int)(i / 11)+0.2f);
+            cube[i].transform.position = pos;
+            cube[i].GetComponent<TextMesh>().color = new Color(1f, 0.5f, 0.5f);
+            isSafe[i] = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        mousePos.x = startPos.x + Input.mousePosition.x/Screen.height*30;
+        mousePos.y = 0.1f;
+        mousePos.z = startPos.y + Input.mousePosition.y/Screen.width*22;
+        
+        if( Input.GetMouseButton(0) ) {
+            index = (int)(( mousePos.z + tileSize / 2 ) / tileSize + 7)*11 // index of y
+            + (int)(( mousePos.x + tileSize / 2 ) / tileSize + 5); //index of x
+            if( index != prevIdx ) {
+                _SetTile(index);
+                print(index);
+                prevIdx = index;
+            }
+        } else if( Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown("s") ) {
+            fp = File.CreateText("./road.txt");
+            print("save txt file");
+            if( fp != null ) {
+                for( int i = 0 ; i < isSafe.Length ; ++i ) {
+                    if( isSafe[i] )
+                        fp.WriteLine(i);
+                }
+                fp.Close();
+            }
+        }
+    }
+    void _SetTile(int idx ) {
+        isSafe[idx] = !isSafe[idx];
+        cube[idx].GetComponent<TextMesh>().color = new Color(isSafe[idx] ? 0.5f : 1f, 0.5f, isSafe[idx] ? 1f : 0.5f ); 
+    }
+}
